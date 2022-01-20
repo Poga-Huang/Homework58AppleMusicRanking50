6730dcdbc01518554e1df441ca8b7cf1ee66bc98
Poga Huang <pgh86812@gmail.com>
2022-01-20 09:25:54 +0800

    Initial Commit

---
+++ Homework58AppleMusicRanking50/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework58AppleMusicRanking50
+//
+//  Created by 黃柏嘉 on 2022/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
